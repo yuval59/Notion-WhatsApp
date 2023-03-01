a026e5502a1f934d28938e6a5cb7ec617b18dfd4
yuval59 <yuvalmaron222@gmail.com>
2023-03-01 02:10:19 +0200

    Style: Added import regions to twitter file

--- src/utils/twitter.ts
+++ src/utils/twitter.ts
@@ -1,4 +1,8 @@
+//#region imports
+//#region internalImports
 import { Tweet } from '../types/types'
+//#endregion
+//#endregion
 
 export async function sendTweet(tweetToSend: Tweet) {
   return new Promise((resolve, reject) =>
